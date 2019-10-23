f47c7ec2fd4bca9dc1bb77d40eb11f78f72f0b8d
Marta Blaszczak <marta.blaszczak1@gmail.com>
2019-10-23 15:45:47 +0100

    Add new version

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// ANIMATION CONTENT ON WEBSITE WHEN SCROLL
+
+AOS.init();
+
+
+//GALLERY
+
+$('[data-fancybox="images"]').fancybox({
+  buttons : [ 
+    'slideShow',
+    'share',
+    'zoom',
+    'fullScreen',
+    'close'
+  ],
+  thumbs : {
+    autoStart : true  
+  }
+
+});
+
+
+// SLOGAN - Wrap every letter in a span
+
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 1550,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 5000
+  });
+
+
+// LOAD MORE GALLERY FUNCTION
+
+function loadGallery() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "See all"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "See less"; 
+    moreText.style.display = "inline";
+  }
+}
