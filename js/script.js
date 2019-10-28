92501f62b54d6943184fe39c35d45ad7010f865d
Marta Blaszczak <marta.blaszczak1@gmail.com>
2019-10-28 11:00:17 +0000

    fixed problem with footer

--- js/script.js
+++ js/script.js
@@ -8,20 +8,20 @@ AOS.init();
 var textWrapper = document.querySelector('.ml3');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
+anime.timeline({loop: false})
   .add({
     targets: '.ml3 .letter',
     opacity: [0,1],
     easing: "easeInOutQuad",
     duration: 1550,
     delay: (el, i) => 50 * (i+1)
-  }).add({
-    targets: '.ml3',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 10000
-  });
+  }).//add({
+    //targets: '.ml3',
+    //opacity: 0,
+    //duration: 1000,
+    //easing: "easeOutExpo",
+    //delay: 1000
+  //});
 
 //GALLERY
 
